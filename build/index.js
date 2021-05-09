6ab2bf6ec304c76bc63a218a2283eda6349ab862
Ayushman Bilas Thakur <ayushmanbilasthakur@gmail.com>
2021-05-09 12:37:51 +0530

    Resetting some things

--- build/index.js
+++ build/index.js
@@ -1,10 +1,46 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 (function (factory) {
     if (typeof module === "object" && typeof module.exports === "object") {
         var v = factory(require, exports);
         if (v !== undefined) module.exports = v;
     }
     else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "discord.js", "./utils/messageHandler", "./utils/commandReader", "mongoose"], factory);
+        define(["require", "exports", "discord.js", "./utils/messageHandler", "./utils/commandReader", "mongoose", "./models/ServerSettings"], factory);
     }
 })(function (require, exports) {
     "use strict";
@@ -14,6 +50,7 @@
     var messageHandler_1 = require("./utils/messageHandler");
     var commandReader_1 = require("./utils/commandReader");
     var mongoose_1 = require("mongoose");
+    var ServerSettings_1 = require("./models/ServerSettings");
     var client = new discord_js_1.Client();
     var prefix = "??";
     mongoose_1.connect(process.env.MONGO_CONNECTION_STRING, {
@@ -31,42 +68,53 @@
     })
         .catch(function (error) { return console.error(error); });
     client.on("ready", function () {
-        client.user.setActivity("wlw?", {
+        client.user.setActivity(prefix, {
             type: "PLAYING",
         });
     });
-    client.on("message", function (message) {
-        //check if bot is mentioned
-        if (message.mentions.has(client.user)) {
-            return message.channel.send("Hi I'm your Watchlist Watcher bot and my prefix is `" + prefix + "`");
-        }
-        //check if the sender is not a bot
-        //check if the message starts with our preset prefix
-        //check if the message is not in the dms
-        try {
-            if (!message.author.bot &&
-                message.content.startsWith(prefix)) {
-                var result = messageHandler_1.default(prefix, message.content);
-                if (!result)
-                    return;
-                if (result.command == "") {
-                    return message.channel.send("Please send a valid command, you can always use \`${prefix}help\` to get started");
-                }
-                if (commandReader_1.default.has(result.command)) {
-                    if (!commandReader_1.default.get(result.command).isDMExecutable && (message.channel instanceof discord_js_1.DMChannel)) {
-                        message.channel.send("To execute this command you need to be in a server");
+    client.on("message", function (message) { return __awaiter(void 0, void 0, void 0, function () {
+        var pf, result;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, ServerSettings_1.default.findOne({ id: message.guild.id })];
+                case 1:
+                    pf = (_a.sent());
+                    if (pf != null && pf.prefix != undefined)
+                        prefix = pf.prefix;
+                    //check if bot is mentioned
+                    if (message.mentions.has(client.user)) {
+                        return [2 /*return*/, message.channel.send("Hi I'm your Watchlist Watcher bot and my prefix is `" + prefix + "`")];
                     }
-                    else {
-                        commandReader_1.default.get(result.command).execute(message, result);
+                    //check if the sender is not a bot
+                    //check if the message starts with our preset prefix
+                    //check if the message is not in the dms
+                    try {
+                        if (!message.author.bot &&
+                            message.content.startsWith(prefix)) {
+                            result = messageHandler_1.default(prefix, message.content);
+                            if (!result)
+                                return [2 /*return*/];
+                            if (result.command == "") {
+                                return [2 /*return*/, message.channel.send("Please send a valid command, you can always use \`${prefix}help\` to get started")];
+                            }
+                            if (commandReader_1.default.has(result.command)) {
+                                if (!commandReader_1.default.get(result.command).isDMExecutable && (message.channel instanceof discord_js_1.DMChannel)) {
+                                    message.channel.send("To execute this command you need to be in a server");
+                                }
+                                else {
+                                    commandReader_1.default.get(result.command).execute(message, result);
+                                }
+                            }
+                            else {
+                                message.channel.send("This command is unkonwn to me, see my docs with the help of the `" + prefix + "help` command");
+                            }
+                        }
                     }
-                }
-                else {
-                    message.channel.send("This command is unkonwn to me, see my docs with the help of the `" + prefix + "help` command");
-                }
+                    catch (error) {
+                        console.error(error);
+                    }
+                    return [2 /*return*/];
             }
-        }
-        catch (error) {
-            console.error(error);
-        }
-    });
+        });
+    }); });
 });
